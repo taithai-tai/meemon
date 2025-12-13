b627c6b85d0fd4ecc958f4ee560c9e1d18efc4fc
taithai <55917846+taithai-tai@users.noreply.github.com>
2025-12-14 04:01:36 +0700

    Update config.js

--- app/config.js
+++ app/config.js
@@ -1,3 +1,3 @@
 // 🔥 แก้ URL ที่นี่ที่เดียวพอ
 window.MEEMON_API_URL =
-  "https://script.google.com/macros/s/AKfycbzVd-VNUeXbmecyTk1ITZOssdNm7uTb0ZFFHkcPgmjJaa6iJM56QLiwA9iKhG8HW3M/exec";
+  "https://script.google.com/macros/s/AKfycbxagZ3CyoVVrMmBDT8NX6CusTcMNTL9SonXXtKGaZv0gx-oVdAKpuye7PMV7RgFIphs/exec";
