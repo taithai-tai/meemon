f837b1d67ebeb3060f9c3b3b6cb6bc81505b4c71
taithai <55917846+taithai-tai@users.noreply.github.com>
2026-02-01 20:21:54 +0700

    Update index.tsx

--- card/home/vervion/1.2/index.tsx
+++ card/home/vervion/1.2/index.tsx
@@ -1,15 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// This file is handled in index.html for better runtime compatibility on GitHub Pages.
+export {};
